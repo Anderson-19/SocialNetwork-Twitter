0ef93e26fa2d97c0795b19ad2973e4b57b827e2d
Anderson-19 <andersonpantor@gmail.com>
2024-02-28 09:05:40 -0400

    Helper para acceder a las varibles de entorno

---
+++ server/src/helpers/envs.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config';
+import { get } from 'env-var';
+
+
+export const envs = {
+
+  PORT: get('PORT').required().asPortNumber(),
+  JWT_SECRET: get('JWT_SECRET').required().asString(),
+  CLOUDINARY_URL: get('CLOUDINARY_URL').required().asString(),
+
+}
