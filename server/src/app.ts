87d9585bd15869b590d821f1febbeac27b57351e
Anderson-19 <andersonpantor@gmail.com>
2024-02-28 09:13:54 -0400

    Instalación de nuevas librerias y arrancando el server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,16 @@
-console.log("Hola Mundo 1");
+import { envs } from './helpers/envs';
+import { AppRoutes } from './routes/AppRoutes';
+import { Server } from './server/server';
+
+(async()=> {
+  main();
+})();
+
+function main() {
+
+  const server = new Server({ port: envs.PORT });
+
+  server.setRoutes( AppRoutes.routes );
+
+  server.start();
+};
