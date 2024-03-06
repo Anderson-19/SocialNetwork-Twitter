ce726ac316b984534771c5dcc5cc7ee75a5683f2
Anderson-19 <andersonpantor@gmail.com>
2024-03-06 17:37:52 -0400

    Lugar de los componentes de uso global

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
 import { useUserStore } from './store';
+import { TopMenu, Sidebar } from './shared';
 
 export const App: FC = () => {
-  
-  const user = useUserStore( state => state.user );
+
+  const user = useUserStore(state => state.user);
 
   if (!user.isLogged) {
     return <Navigate to="/" />;
@@ -14,13 +15,17 @@ export const App: FC = () => {
     <div className="p-3 h-screen w-full bg-blue-600" >
       <div className=" h-full bg-white overflow-hidden flex flex-col rounded-xl shadow-xl">
         {/* <!-- navbar --> */}
-
+        <TopMenu avatar={user.avatar!} />
         <div className="h-full flex">
           {/* <!-- slidebar 1 --> */}
           <div className="h-full w-1/2 pt-10 px-5 "></div>
 
+          <Sidebar />
+
           <Outlet />
-          
+          <div className="h-full w-screen border-r pt-8 px-5">
+            {/* <UsersForFollow /> */}
+          </div>
 
         </div>
       </div>
