e3e338d19f8cff989b28b439d44d0f9c6bcdc9f4
Anderson-19 <andersonpantor@gmail.com>
2024-03-06 22:18:42 -0400

    post.interface creada

--- client/src/interfaces/index.ts
+++ client/src/interfaces/index.ts
@@ -1,2 +1,3 @@
 export * from './errorApi.interface';
 export * from './user.interface';
+export * from './post.interface';
---
+++ client/src/interfaces/post.interface.ts
@@ -0,0 +1,16 @@
+export interface Post {
+    ok?: boolean;
+    post_id?: string;
+    content?: string;
+    img?: string;
+    created_at?: string;
+    user_id?: string;
+    following_id?: string
+
+    name?: string;
+    lastname?: string;
+    email?: string;
+    username?: string;
+    avatar?: string;
+}
+
