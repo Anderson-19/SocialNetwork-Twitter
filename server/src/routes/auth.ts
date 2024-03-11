4834f0aed725ab85b6240a9503d75ed519df597b
Anderson-19 <andersonpantor@gmail.com>
2024-03-11 17:21:32 -0400

    Modificación en el nombre de la ruta donde el usuario se registra

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -18,14 +18,14 @@ export class AuthRoutes {
       middlewaresValidators.validateField
     ], AuthController.logIn);
 
-    router.post('/singIn', [
+    router.post('/register', [
       check('name', 'Name is required').not().isEmpty(),
       check('lastname', 'Lastname is required').not().isEmpty(),
       check('username', 'Username is required').not().isEmpty(),
       check('email', 'Email is required').isEmail(),
       check('password', 'Password is required').not().isEmpty(),
       middlewaresValidators.validateField
-    ], AuthController.singIn);
+    ], AuthController.register);
 
     router.post('/logout', AuthController.logout);
 
